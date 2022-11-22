62256925cb76202a41d4f3477db1a9abbe1326c1
Emma Campbell <emma@emmacampbell.dev>
2022-11-22 15:43:35 -0500

    🐛 og image generator

--- lib/createOgImage.ts
+++ lib/createOgImage.ts
@@ -30,12 +30,12 @@ export const createOgImage = ({
 
     // PROFILE IMAGE
     // dynamically fetched from my twitter profile
-    `l_twitter_name:delba_oliveira`,
+    `l_twitter_name:emmacampbell114`,
     // Transformations
     `c_thumb,g_face,r_max,w_380,h_380,q_100`,
     // Positioning
     `fl_layer_apply,w_140,g_north_west,x_100,y_100`,
 
     // BG
-    `grain-gradient.png`,
+    `og_image_background_gesaa1.png`,
   ].join("/");
