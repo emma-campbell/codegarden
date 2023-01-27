aaf05502abfe4fc13436bfa3bb856b41671f40f1
Emma Campbell <emma@emmacampbell.dev>
2023-01-27 15:24:03 -0500

    fix: tag wrap #70

--- ui/tag-list.tsx
+++ ui/tag-list.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import { Tag } from "contentlayer/generated";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 type TagListProps = {
   tags: Tag[];
@@ -23,19 +23,19 @@ const getNextGradient = () => {
 
 export const TagList: FC<TagListProps> = ({ tags }) => {
   return (
-    <div className="flex flex-row flex-wrap space-x-4">
+    <div className="flex flex-wrap">
       {tags.map((t) => {
         return (
           <div
             key={t.slug}
             className={classNames(
               "transform transition-all",
-              "rounded-xl bg-gradient-to-r p-1 shadow-surface-elevation-high",
+              "rounded-xl bg-gradient-to-r p-1 shadow-surface-elevation-high mr-2 mt-2",
               getNextGradient()
             )}
           >
-            <div className="flex flex-col bg-black rounded-lg px-4 py-2 justify-between h-full">
-              <p className="font-bold">{t.title}</p>
+            <div className="bg-black rounded-lg px-4 py-2 justify-between h-full">
+              <p className="font-medium text-sm">{t.title}</p>
             </div>
           </div>
         );
