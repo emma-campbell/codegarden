d6e8542b58ae73aba4316e447731e3abcc771008
Emma Campbell <emma@emmacampbell.dev>
2023-02-03 12:22:02 -0500

    fix: mdx stylings with new color scheme

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const FOCUS_VISIBLE_OUTLINE = `focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white`;
 
-export const LINK_STYLES = `text-white font-medium underline decoration-white underline-offset-2 transition-all hover:text-green-200 hover:decoration-green-200`;
+export const LINK_STYLES = `text-white font-medium underline decoration-white underline-offset-2 transition-all hover:text-purple hover:decoration-purple`;
 
-export const LINK_SUBTLE_STYLES = `hover:underline hover:decoration-green-300 hover:underline-offset-2 hover:text-green-200`;
+export const LINK_SUBTLE_STYLES = `hover:underline hover:decoration-purple hover:underline-offset-2 hover:text-purple`;
 
 export const HEADING_LINK_ANCHOR = `before:content-['#'] before:absolute before:-ml-[1em] before:text-white/0 hover:before:text-white/50 pl-[1em] -ml-[1em]`;
