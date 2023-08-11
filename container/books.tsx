750a9f10d6109e55ae37eaffff529e7e00a3ba0a
Emma Campbell <emma@emmacampbell.dev>
2023-08-11 17:14:34 -0400

    update book formatting

--- container/books.tsx
+++ container/books.tsx
@@ -2,10 +2,34 @@
 
 import { Accordion, AccordionItem } from "@/ui/accordion";
 import { BookListItem } from "@/ui/book-list";
+import { Book } from "contentlayer/generated";
+import moment from "moment";
+
+function sortDate(a: string, b: string): number {
+  const c = moment(a);
+  const d = moment(b);
+
+  if (c.isBefore(d)) {
+    return 1;
+  } else if (c.isSame(d)) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
 
 export const Books = ({ books }) => {
-  const reading = books?.filter((b) => b.status === "in progress");
-  const complete = books?.filter((b) => b.status === "completed");
+  const reading = books?.filter((b: Book) => b.status === "in progress");
+  const complete = books
+    ?.filter((b: Book) => b.status === "completed")
+    .sort((a: Book, b: Book) => {
+      if (a.start && b.start) {
+        return sortDate(a.start, b.start);
+      } else {
+        return -1;
+      }
+    })
+    .reverse();
   const queued = books?.filter((b) => b.status === "queued");
 
   return (
