592a83ff171914970d3c3382ac50acec9596b5cc
Emma Campbell <emma@emmacampbell.dev>
2024-04-11 19:08:14 -0400

    chore: redirect blog pages to spooklore

---
+++ app/blog/[slug]/route.ts
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const slug = path.replace("/blog/", "");
+
+  redirect(`https://spooklore.com/notebook/${slug}`);
+}
